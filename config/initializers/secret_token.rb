13083ee69317d61a1d1f7a0d7c034bd0c1b449c4
Frederick Marshal <fmarshal@gmail.com>
2013-04-23 04:02:00 +0700

    Baru saja mengupdate segalanya nambah simple_form nambah login/logut

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,11 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Treebook::Application.config.secret_key_base = '3a3a9344be373b82eb54a92a5cd9817b5fbc97aa2a822aa385f5b419ed1bd9dd6d94d625a17cc8a209f1454b185ed944f886abc216ce65bf5c445395f3586ef9'
+Treebook::Application.config.secret_token = '33556d6265e114f2a48ffc4d185dd9e27b33c4a43fb007bf48f0fbb1c7c2bc41b457aa0e95bd82449e5f85b5f6798690acc9c10e5eb512409cbb670d82d47fc5'
